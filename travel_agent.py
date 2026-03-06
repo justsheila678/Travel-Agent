06fa7f804dd82ff380e6756d0bad88a13c268b39
justsheila678 <smercado2468@gmail.com>
2026-03-06 09:30:43 -0400

    Added tabs and new column for country suggestions!

--- travel_agent.py
+++ travel_agent.py
@@ -2,8 +2,9 @@ import flet as ft
 import requests
 
 def get_country_info(name):
+    global country_link
     country_link = f"https://restcountries.com/v3.1/name/{name}?fullText=true"
-    
+
     try:
         response = requests.get(country_link)
         if response.status_code != 200: 
@@ -41,47 +42,148 @@ def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.DARK
     page.scroll = "adaptive"
 
-    search_input = ft.TextField(label="Enter Country Name", width=400)
-    results_area = ft.Column(visible=False, spacing=15)
+    #COUNTRY INFORMATION TAB -------------------------------------------------------------------------
 
-    def handle_search(e):
-        results_area.visible = False
+    search_input = ft.TextField(label="Enter Country Name", width=400)
+    suggestions1 = ft.Column(spacing=5)
+    results_area = ft.Column(visible=False, spacing=15)
+    results_area.visible = False
+    page.update()
+
+    def select_country(e):
+        country_name = search_input.value.strip().lower()
+        if len(country_name) < 2:
+            suggestions1.controls = []
+            page.update()
+            return
+
+        try:
+            suggestions1.controls = []
+            responsee = requests.get(f"https://restcountries.com/v3.1/name/{country_name}")
+            dataa = responsee.json()
+
+            for country in dataa[:5]:
+                name = country["name"]["common"]
+                suggestions1.controls.append(
+                    ft.TextButton(text=name,
+                    on_click=lambda e, n=name: choose_country(n))
+                    )
+            page.update()
+        except:
+            suggestions1.controls = []
+            page.update()
+            
+    #this part will set destination to the option the user clicks from suggestions list.
+    def choose_country(name):
+        search_input.value = name
+        suggestions1.controls = []
         page.update()
 
-        country_data = get_country_info(search_input.value)
+    def handle_search(e):
+
+        country_data = get_country_info(search_input.value.strip())
 
         if country_data:
             results_area.controls = [
-                ft.Image(src=country_data["flag"], width=250, border_radius=10),
-                ft.Text(country_data["official_name"], size=28, weight="bold"),
-                ft.Text(f"📍 Capital: {country_data['capital']} | 🌡️ Weather: {country_data['weather']}", 
-                        size=20, color="blue", weight="w600"),
-                ft.Divider(),
-                ft.Text(f"Region: {country_data['region']}"),
-                ft.Text(f"Population: {country_data['population']}"),
-                ft.Text(f"Currency: {country_data['currency']}"),
-                ft.Text(f"Languages: {country_data['languages']}"),
-                ft.Text(f"Time Zones: {country_data['timezones']}"),
-                ft.Text(f"Country Code: {country_data['code']}")
-            ]
-        else:
+                    ft.Image(src=country_data["flag"], width=250, border_radius=10),
+                    ft.Text(country_data["official_name"], size=28, weight="bold"),
+                    ft.Text(f"📍 Capital: {country_data['capital']} | 🌡️ Weather: {country_data['weather']}", 
+                            size=20, color="blue", weight="w600"),
+                        ft.Divider(),
+                        ft.Text(f"Region: {country_data['region']}"),
+                        ft.Text(f"Population: {country_data['population']}"),
+                        ft.Text(f"Currency: {country_data['currency']}"),
+                        ft.Text(f"Languages: {country_data['languages']}"),
+                        ft.Text(f"Time Zones: {country_data['timezones']}"),
+                        ft.Text(f"Country Code: {country_data['code']}")
+                    ]
 
-            results_area.controls = [
-                ft.Text(f"Error: Could not find '{search_input.value}'. Please check the spelling.", 
-                        color=ft.Colors.RED_700, size=18, weight="bold")
-            ]
-
-        results_area.visible = True
+            results_area.visible = True
         page.update()
+    
+    search_input.on_change = select_country
 
-    page.add(
+    #END OF COUNTRY INFORMATION TAB ---------------------------------------------------------------
+
+    #Adding country information stuff to tab content
+    tab1content = ft.Column([
         ft.Text("🌍 Travel Agency Search Portal", size=32, weight="bold"),
-        ft.Row([
-            search_input, 
-            ft.ElevatedButton("Search", icon=ft.Icons.SEARCH, on_click=handle_search)
-        ]),
-        ft.Divider(height=30),
+        search_input,
+        suggestions1,
+        ft.ElevatedButton("Search", icon=ft.Icons.SEARCH, on_click=handle_search),
+        ft.Divider(),
         results_area
+    ],
+    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+    spacing=15
+)   
+    
+    #TRAVEL PLAN TAB ----------------------------------------------------------------------------------
+
+    #getting client information.
+    client_name = ft.TextField(hint_text="Enter Full Name", width=400)
+    destination_country = ft.TextField(hint_text="Enter Destination Country", width=400)
+    suggestions = ft.Column(spacing=5)
+
+    #function to get country suggestions as user types in destination text field
+    def select_destination(e):
+        #logic of this part: country_value is the stipped and lowercase value of what the user wrote in textfield. 
+        #when the user writes up to 2 characters (len(country_value) will count this), suggestions turns into a list 
+        #of the countries that match the country_value value
+        country_value = destination_country.value.strip().lower()
+        if len(country_value) < 2:
+            suggestions.controls=[]
+            page.update()
+            return
+        
+        try:
+            suggestions.controls = []
+            responsee = requests.get(f"https://restcountries.com/v3.1/name/{country_value}")
+            dataa = responsee.json()
+
+            for country in dataa[:5]:
+                name = country["name"]["common"]
+                suggestions.controls.append(
+                    ft.TextButton(text=name,
+                    on_click=lambda e, n=name: choose_country(n))
+                    )
+            page.update()
+        except:
+            suggestions.controls = []
+            page.update()
+        
+        #this part will set destination to the option the user clicks from suggestions list.
+        def choose_country(name):
+            destination_country.value = name
+            suggestions.controls = []
+            page.update()
+
+    destination_country.on_change = select_destination
+
+
+    #adding to 2nd tab content
+    tab2content = ft.Column(
+        [
+            ft.Text("Create Travel Plan", size=28, weight="bold"),
+            client_name,
+            destination_country,
+            suggestions
+        ],
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        spacing=15
     )
+
+    #END OF TRAVEL PLAN TAB ---------------------------------------------------------------------------
+
+    #Creating tabs
+    tabs = ft.Tabs(selected_index=0,
+                   animation_duration=300,
+                     tabs=[
+                          ft.Tab(text="Country Search", content=tab1content),
+                          ft.Tab(text="Travel Plan", content=tab2content)
+                     ])
+
+    #Adding tabs
+    page.add(tabs)
 
 ft.app(target=main)
